ca626109e5c0da2931264ecda38c3243f329d4fd
Ale Rimoldi <ale@graphicslab.org>
2015-02-15 21:12:18 +0100

    on the live site, it's not possible to render the communiqués as list. just render paragraphs

--- htdocs/template/page_actualite_communiques.php
+++ htdocs/template/page_actualite_communiques.php
@@ -1,5 +1,11 @@
-<?php // Aoloe\debug('table', $table); ?>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
 
 <?php foreach ($table as $item) : ?>
-- <strong><?= $item[0] ?></strong><br /><a href="<?= $item[1] ?>"><?= $item[2] ?></a> (<?= $item[3] ?>)</li>
+**<?= $item[0] ?>**  
+[<?= $item[2] ?>](<?= $item[1] ?>)
+
 <?php endforeach; ?>
+
