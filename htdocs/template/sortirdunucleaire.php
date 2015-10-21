025d4d324ddda2ba5e9c7b3a43cc6a4024d784cf
Ale Rimoldi <ale@graphicslab.org>
2015-10-21 21:06:44 +0200

    allow scaling of the page

--- htdocs/template/sortirdunucleaire.php
+++ htdocs/template/sortirdunucleaire.php
@@ -3,7 +3,7 @@
 <head>
 
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title><?= isset($title) ? $title.' - ' : '' ?><?= isset($title_site) ? $title_site : ''?></title>
 
